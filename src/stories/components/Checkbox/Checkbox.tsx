56c2102a84b67df51828959a6dc3c69843778abc
hda1994 <hda1994@gmail.com>
2021-12-02 14:53:40 +0400

    update checkbox

--- src/stories/components/Checkbox/Checkbox.tsx
+++ src/stories/components/Checkbox/Checkbox.tsx
@@ -5,20 +5,44 @@ import './checkbox.css';
 // import styles from './Button.module.css';
 
 interface CheckboxProps {
+    checked?: boolean;
     disabled?: boolean;
     label: string;
 }
 
 export const Checkbox = ({
+                             checked = false,
                              disabled = false,
                              label = '',
                              ...props
                          }: CheckboxProps) => {
-    const [pressed, setPressed] = useState('');
+    const [isChecked, setIsChecked] = useState(checked);
+    const [pressed, setPressed] = useState(false);
+    const onClickHandler = (event: any) => {
+        event.preventDefault();
+        if(!disabled) {
+            setIsChecked(!isChecked);
+        }
+    }
+    const onPressHandler = (press: boolean) => {
+        if(!disabled) {
+            setPressed(press);
+        }
+    }
     return (
-        <label className="checkbox--container">
+        <label
+            className={[
+                'checkbox--container',
+                `${isChecked ? 'checked' : ''}`,
+                `${pressed ? 'pressed' : ''}`,
+                `${disabled ? 'disabled' : ''}`
+            ].join(' ')}
+            onClick={onClickHandler}
+            onMouseDown={() => onPressHandler(true)}
+            onMouseUp={() => onPressHandler(false)}
+            onMouseOut={() => onPressHandler(false)}>
             {label}
-            <input type="checkbox" className="checkbox--input"/>
+            <input type="checkbox" className="checkbox--input" checked={isChecked} disabled={disabled}/>
             <span className="checkbox--checkmark"/>
         </label>
     );
